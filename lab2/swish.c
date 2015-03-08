7940b2ecfca9c7c2964ab1b6fbefbce28e03df8b
Christopher John Ryan 108088117 <student@vl72.cs.stonybrook.edu>
2015-03-08 17:16:24 -0400

    CD works for a hardcoded 'directory' string. LS works PWD works
    INVALID COMMANDS WILL CRASH PROGRAM.

--- lab2/swish.c
+++ lab2/swish.c
@@ -3,58 +3,102 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include "mysyscall.h"
 
 // Assume no input line will be longer than 1024 bytes
-#define MAX_INPUT 1024
+#define MAX_INPUT_BUFF_BUFF 1024
 
-int 
-main (int argc, char ** argv, char **envp) {
+extern char **environ;
+
+
+int main (int argc, char ** argv, char **envp) {
 
   int finished = 0;
   char *prompt = "swish> ";
-  char cmd[MAX_INPUT];
+  char cmd[MAX_INPUT_BUFF_BUFF];
   char *EXIT_CMD =  "exit\n";
+   char cwd[MAX_INPUT_BUFF_BUFF]; 
+  /********************************************************************
+   Setting the commands that the shell recognizes 
+   ********************************************************************/
+  char *commandsNL[] = { "ls\n","cd\n","pwd\n"}; 
+  char *commands[] = { "ls","cd","pwd"}; 
+  char *directory = "testDir";
 
   while (!finished) {
     char *cursor;
     char last_char;
     int rv;
     int count;
+    getcwd(cwd,sizeof(cwd));
 
 
     // Print the prompt
     rv = write(1, prompt, strlen(prompt));
-   if (!rv) { 
+    int iterator =0;
+    int pid=-1;
+
+
+    for(iterator= 0; iterator < 3; iterator++){
+      if (!strncmp(commandsNL[iterator],cmd,2)){
+
+          if (strncmp(commandsNL[1],cmd,2) == 0){
+            if (chdir(directory) < 0)
+            {
+              printf("error opening directory");
+              return 123;
+
+            } else {
+
+              if ((pid = fork()) < 0) printf("fork failed");
+                  if (pid == 0){
+                    execvp("pwd",argv);
+                    
+                    }
+            }
+
+
+          }
+      if ((pid = fork()) < 0) printf("fork failed");
+        if (pid == 0){
+          execvp(commands[iterator],argv);
+          printf("Executing commands[iterator]");
+          
+          }
+
+        }
+      }
+     
+    /********************************************************************
+    Removing this should make the only exit occur from the proper exit.
+    IE typing 'exit'.
+    *********************************************************************
+    if (!rv) { 
      finished = 1;
       break;
-   }
+    } Ends while loop */
     
-    // read and parse the input
-    for(rv = 1, count = 0, 
-	  cursor = cmd, last_char = 1;
-	rv 
-	  && (++count < (MAX_INPUT-1))
-	  && (last_char != '\n');
-	cursor++) { 
+    
+    /* read and parse the input put it into CMD */
+    for(rv = 1, count = 0, cursor = cmd, last_char = 1;rv && (++count < (MAX_INPUT_BUFF_BUFF-1)) && (last_char != '\n'); cursor++) { 
 
       rv = read(0, cursor, 1);
       last_char = *cursor;
     } 
+   
     *cursor = '\0';
-	if(strcmp(cmd, EXIT_CMD) == 0) { printf("Exiting...\n"); rv = 0; }
+	  if(strcmp(cmd,EXIT_CMD) == 0) { execvp("exit",argv); return 0; }
 
 
-    if (!rv) { 
-      finished = 1;
-      break;
-    }
 
+  
 
     // Execute the command, handling built-in commands separately 
     // Just echo the command line for now
-    write(1, cmd, strnlen(cmd, MAX_INPUT));
+    write(1, cmd, strnlen(cmd, MAX_INPUT_BUFF_BUFF));
 
-  }
+  } /* End while */
 
   return 0;
 }
