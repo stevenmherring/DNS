9b0b567ca7474ebc4626ee15b7acf776524990d6
Christopher John Ryan 108088117 <student@vl72.cs.stonybrook.edu>
2015-03-09 16:07:05 -0400

    Fixed zombie process in CD Fixed delayed print of the current directory.

--- lab2/swish.c
+++ lab2/swish.c
@@ -42,61 +42,34 @@ int main (int argc, char ** argv, char **envp) {
   char *commands[] = { "ls","cd","pwd"}; 
   
 
-  while (!finished) {
+
+
+
+
+  /*******************************************************************
+  Get the CWD path
+  Add a space and a null terminator to the path
+  Print the path 
+  Print the 'swish' prompt
+  ********************************************************************/
+  int rv;
   char *theCWD = getcwd(cwd,sizeof(cwd));
-
   char *cursor;
-
   cursor = theCWD + strlen(theCWD);
   *cursor = ' ';
   cursor++;
   *cursor = '\0';
   cursor =NULL ;
+  rv = write(1, theCWD, strlen(theCWD));
+  rv = write(1, prompt, strlen(prompt));
 
 
-
+  while (!finished) {
   char last_char;
-  int rv;
   int count;
   int iterator =0;
   int pid=-1;
 
-
-    for(iterator= 0; iterator < 3; iterator++){
-      if (!strncmp(commandsNL[iterator],cmd,2)){
-          
-          if (strncmp(commandsNL[1],cmd,2) == 0){
-
-          cursor = cmd;
-          cursor+=3;
-          strcpy(cmdTemp,cursor);
-
-          
-          cursor =cmdTemp;
-          cursor+=strlen(cmdTemp)-1;
-          *cursor = '\0'; 
-            if (strcmp(cmd,"cd\n") == 0){
-              printf("Needs to be implemented");
-              return 1;
-            } else 
-                    if (chdir(cmdTemp) < 0)
-                    {
-                      printf("ERROR OPENING DIRECTORY:%s \n",cmdTemp);
-                      return 1;
-
-                    } 
-                
-          }
-      if ((pid = fork()) < 0) printf("fork failed");
-        if (pid == 0){
-          execvp(commands[iterator],argv);
-          printf("Executing commands[iterator]");
-          
-          }
-
-        }
-      }
-     
     /********************************************************************
     Removing this should make the only exit occur from the proper exit.
     IE typing 'exit'.
@@ -115,7 +88,69 @@ int main (int argc, char ** argv, char **envp) {
     } 
    
     *cursor = '\0';
-	  if(strcmp(cmd,EXIT_CMD) == 0) { printf("Exiting. "); finished = 1; return 0; }
+
+    
+  /********************************************************************
+   Check the CMD string to see if the input matches any of the commands
+   found in the commandsNL array.
+   ********************************************************************/
+    for(iterator= 0; iterator < 3; iterator++){
+
+  /********************************************************************
+   If the command is in the array, this line returns 0 so we ! to enter
+   ********************************************************************/
+      if (!strncmp(commandsNL[iterator],cmd,2)){
+          
+
+  /********************************************************************
+   IF the command is CD
+   ********************************************************************/
+          if (strncmp(commandsNL[1],cmd,2) == 0){
+
+          cursor = cmd; //Make a cursor to the CMD string
+          cursor+=3; // Move the cursor past 'cd ' onto the argument for CD
+          strcpy(cmdTemp,cursor); // Copy the value at location into the cmdTemp string (this now holds CD arguments)
+
+          
+          cursor =cmdTemp; // The copied over value still has '\n' so we make a cursor it
+          cursor+=strlen(cmdTemp)-1; // Move the cursor to the end -1 (The location of the '\n')
+          *cursor = '\0';  // Replace the '\n' with Null terminator
+            if (strcmp(cmd,"cd\n") == 0){ // Base case CD needs to implement root CD
+              /* TODO */
+              printf("Needs to be implemented");
+              return 1;
+            } else 
+                    if (chdir(cmdTemp) < 0) /* Fail case of opening directory */
+                    {
+                      printf("ERROR OPENING DIRECTORY:%s \n",cmdTemp);
+                      return 1;
+
+                    } else { 
+                            theCWD = getcwd(cwd,sizeof(cwd));
+                            cursor = theCWD + strlen(theCWD);
+                            *cursor = ' ';
+                            cursor++;
+                            *cursor = '\0';
+                        }
+                
+          }
+      else if ((pid = fork()) < 0) printf("fork failed"); /* Fail case on fork */
+        if (pid == 0){
+          execvp(commands[iterator],argv);
+          printf("Executing commands[iterator]");
+          
+          }
+
+        }
+      }
+     
+
+
+	  if(strncmp(cmd,EXIT_CMD,4) == 0) { 
+      printf("Exiting. "); 
+      finished = 1; 
+      return 0; 
+    }
 
 
 
