64aad9bded3bfb036a4176c99d28a9339024a2df
Christopher John Ryan 108088117 <student@vl72.cs.stonybrook.edu>
2015-03-09 23:01:41 -0400

    Added environment variables Added capability for CD to home Fixed tokenization Added support for command arguments

--- lab2/swish.c
+++ lab2/swish.c
@@ -1,4 +1,4 @@
-/* CSE 306: Sea Wolves Interactive SHell */
+/* CSE 306: Sea Wolves Interactive Shell */
 
 #include <stdio.h>
 #include <string.h>
@@ -6,6 +6,10 @@
 #include <errno.h>
 #include "mysyscall.h"
 #include "swishHelperFunctions.c"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
 
 
 
@@ -20,28 +24,27 @@ int main (int argc, char ** argv, char **envp) {
 
 
 
+
   /********************************************************************
   Command line parsing begins here
   First check to see if -d is defined, and debugging messages need to be printed.
    ********************************************************************/
 
+
+  putenv("SomeVariable=SomeValue");
   int finished = 0;
   char *prompt = "swish> ";
   char cmd[MAX_INPUT_BUFF_BUFF];
+  char tokenBuff[MAX_INPUT_BUFF_BUFF];
   char cmdTemp[MAX_INPUT_BUFF_BUFF];
   char *EXIT_CMD =  "exit\n";
   char cwd[MAX_INPUT_BUFF_BUFF]; 
   /********************************************************************
    Setting the commands that the shell recognizes 
    ********************************************************************/
-  char *commandsNL[] = { "ls\n","cd\n","pwd\n"}; 
-  char *commands[] = { "ls","cd","pwd"}; 
+  char *commandsNL[] = { "ls\n","cd\n","pwd\n","printenv\n"}; 
+  //char *commands[] = { "ls","cd","pwd","make"}; 
   
-
-
-
-
-
   /*******************************************************************
   Get the CWD path
   Add a space and a null terminator to the path
@@ -56,7 +59,6 @@ int main (int argc, char ** argv, char **envp) {
   cursor++;
   *cursor = '\0';
   cursor =NULL ;
-  getCMDargs(commandsNL,1,argv,argc);
 
   rv = write(1, theCWD, strlen(theCWD));
   rv = write(1, prompt, strlen(prompt));
@@ -78,28 +80,45 @@ int main (int argc, char ** argv, char **envp) {
     
     
     /* read and parse the input put it into CMD */
-    for(rv = 1, count = 0, cursor = cmd, last_char = 1;rv && (++count < (MAX_INPUT_BUFF_BUFF-1)) && (last_char != '\n'); cursor++) { 
+  for(rv = 1, count = 0, cursor = cmd, last_char = 1;rv && (++count < (MAX_INPUT_BUFF_BUFF-1)) && (last_char != '\n'); cursor++) { 
 
-      rv = read(0, cursor, 1);
-      last_char = *cursor;
-    } 
+    rv = read(0, cursor, 1);
+    last_char = *cursor;
+
+
+  }
+
+  *cursor = '\0';
    
-    *cursor = '\0';
-  //Tokenize the commands by a space deilmeter store it into *p
-  char cmdcpy[MAX_INPUT_BUFF_BUFF];
-  strncpy(cmdcpy, cmd, strlen(cmd));
-//  char *p = strtok(cmdcpy, " ");
+  strcpy(tokenBuff,cmd);
+  //printf("TokenBuff pre Tokenizer : %s \n",tokenBuff);
+  char *tokenArr[50];
+  int counter = 0;
+  char* token = strtok(tokenBuff, " ");
+  while (token) {
+      tokenArr[counter++] = token;
+      token = strtok(NULL, " ");
+  }
+  tokenArr[counter] = NULL;
 
+  int index = 0;
+  for(index = 0; tokenArr[counter - 1][index] != '\0'; index++) {
+    if(tokenArr[counter - 1][index] == '\n') {
+      printf("Token: %s",tokenArr[counter - 1]);
+      tokenArr[counter - 1][index] = '\0';
+      break;
+    }
+  }
   /********************************************************************
    Check the CMD string to see if the input matches any of the commands
    found in the commandsNL array.
-+-   ********************************************************************/
-    for(iterator= 0; iterator < 3; iterator++){
+  ********************************************************************/
+    for(iterator= 0; iterator < 5; iterator++){
 
   /********************************************************************
    If the command is in the array, this line returns 0 so we ! to enter
    ********************************************************************/
-      if (!strncmp(commandsNL[iterator],cmd,2)){
+      if (!strcmp(commandsNL[iterator],cmd)){
           
 
   /********************************************************************
@@ -108,25 +127,30 @@ int main (int argc, char ** argv, char **envp) {
           if (strncmp(commandsNL[1],cmd,2) == 0){
 
           cursor = cmd; //Make a cursor to the CMD string
-          cursor+=3; // Move the cursor past 'cd ' onto the argument for CD
-          strcpy(cmdTemp,cursor); // Copy the value at location into the cmdTemp string (this now holds CD arguments)
+          cursor += 3; // Move the cursor past 'cd ' onto the argument for CD
+          strcpy( cmdTemp , cursor ); // Copy the value at location into the cmdTemp string (this now holds CD arguments)
 
           
-          cursor =cmdTemp; // The copied over value still has '\n' so we make a cursor it
-          cursor+=strlen(cmdTemp)-1; // Move the cursor to the end -1 (The location of the '\n')
+          cursor = cmdTemp; // The copied over value still has '\n' so we make a cursor it
+          cursor += strlen(cmdTemp)-1; // Move the cursor to the end -1 (The location of the '\n')
           *cursor = '\0';  // Replace the '\n' with Null terminator
-            if (strcmp(cmd,"cd\n") == 0){ // Base case CD needs to implement root CD
-              /* TODO */
-              printf("Needs to be implemented");
-              return 1;
+            if (strcmp( cmd , "cd\n") == 0){ // Base case CD needs to implement root CD
+             
+              
+               chdir(getenv("HOME"));
+              theCWD = getcwd (cwd,sizeof(cwd));
+              cursor = theCWD + strlen(theCWD);
+              *cursor = ' ';
+              cursor++;
+              *cursor = '\0';
             } else 
-                    if (chdir(cmdTemp) < 0) /* Fail case of opening directory */
+                    if ( chdir ( cmdTemp ) < 0) /* Fail case of opening directory */
                     {
                       printf("ERROR OPENING DIRECTORY:%s \n",cmdTemp);
                       return 1;
 
                     } else { 
-                            theCWD = getcwd(cwd,sizeof(cwd));
+                            theCWD = getcwd (cwd,sizeof(cwd));
                             cursor = theCWD + strlen(theCWD);
                             *cursor = ' ';
                             cursor++;
@@ -135,15 +159,17 @@ int main (int argc, char ** argv, char **envp) {
                 
           }
 //if the input is for a application (cat/ls) then we need to parse the arguments following it
-      else if ((pid = fork()) < 0) printf("fork failed"); /* Fail case on fork */
+      else if ((pid = fork()) < 0){ printf("fork failed"); return 1; }/* Fail case on fork */
         if (pid == 0){
-	int j = 0, k = 0;
-	char input[MAX_INPUT_BUFF_BUFF];
-	char output[MAX_INPUT_BUFF_BUFF]; 
-	for(j = 0; j < strlen(cmd); j++) {
-	/**
-	* Search for <, >, or |
-	**/
+  
+      	int j = 0, k = 0;
+      	char input[MAX_INPUT_BUFF_BUFF];
+      	char output[MAX_INPUT_BUFF_BUFF]; 
+      	for(j = 0; j < strlen(cmd); j++) {
+      	/**
+      	* Search for <, >, or |
+      	**/
+
 	if(cmd[j] == '<') {
 		int l = 0;
 		for(k = j+1; k < strlen(cmd); k++) {
@@ -165,8 +191,15 @@ int main (int argc, char ** argv, char **envp) {
 	printf("Input: %s\n", input);
 	printf("Output: %s\n", output);
          
-          execvp(commands[iterator],argv);
+          // execvp(commands[iterator],tokenArr);
+          execvp(tokenArr[0], tokenArr);
+          cursor = cmd;
+          *cursor = '\n';
           
+          } else {
+            /* in parent */
+            // int status;
+            waitpid(pid, NULL /*&status*/, WUNTRACED | WCONTINUED);
           }
 
         }
@@ -193,8 +226,11 @@ int main (int argc, char ** argv, char **envp) {
   Print the CWD
   Print the prompt
   ********************************************************************/
+
   rv = write(1, theCWD, strlen(theCWD));
   rv = write(1, prompt, strlen(prompt));
+
+  memset(cmd,'\0',MAX_INPUT_BUFF_BUFF);
 
   } /* End while */
 
--- lab2/swishHelperFunctions.c
+++ lab2/swishHelperFunctions.c
@@ -1,8 +1,10 @@
+#define MAX_INPUT_BUFFER 1024
+
 /*
 Helper functions for swish
 */
 
-int getCMDargs(char** commandsNL,int commandsNLchoice, char ** argv, int argc){
+int getArgs(char ** argv, int argc){
 	int i =0;
 	for (i =0; i<argc; i++){
 
@@ -10,3 +12,25 @@ int getCMDargs(char** commandsNL,int commandsNLchoice, char ** argv, int argc){
 	}
 	return 0;
 }
+/*
+char* getCMDargs(char* command,char* bufferToParse){
+	int i =0;
+	char *cmdArgs[MAX_INPUT_BUFFER];
+	strcpy(cmdArgs,bufferToParse);
+	char *cursor = cmdArgs;
+	for (i=0; i<strlen(bufferToParse);i++){
+		if (*cursor == ' '){
+			cursor++;
+			printf("%s",cursor);
+		}
+		cursor+=i;
+	}
+}
+
+int parseExecFlags(char** commands,int commandsNLchoice, char ** argv, int argc){
+
+	//getCMDargs();
+
+    //execvp(commands[iterator],argv);
+	return 0;
+} */
