b7d67dbcbb4166112f59ca24a466d845e5f2d4d5
Steven Herring <steven.herring@stonybrook.edu>
2015-03-11 18:21:01 -0400

    Built helper function for recursive piping

--- lab2/redirHelper.c
+++ lab2/redirHelper.c
@@ -1,6 +1,6 @@
 void redirInput(char *input);
 void redirOutput(char *input);
-
+void recursivePipe(char *pipes[], int index, int fd_in);
 /**
 *redirInput small method for handling input redirection
 **/
@@ -20,3 +20,38 @@ void redirOutput(char *input) {
 	dup2(fd_in, STDOUT_FILENO);
 	close(fd_in);
 }
+
+/**
+*recursivePipe method uhhhh...i wrote it by hand so no comments
+**/
+void recursivePipe(char *pipes[], int index, int fd_in) {
+	if(pipes[index+1] == NULL) {
+		if(fd_in != STDIN_FILENO) {
+			if(dup2(fd_in, STDIN_FILENO) != -1) {
+				close(fd_in);
+			} else {
+			//space for error handling
+			}
+		}
+		execlp(pipes[index], pipes[index], NULL);
+	} else {
+		int fd[2];
+		pid_t cpid;
+		if((pipe(fd) == -1) || ((cpid = fork()) -- -1)) {
+			//error handle, bad fork
+		}
+		if(cpid == 0) {
+			isChild = 1;
+			close(fd[0]);
+			if(dup2(fd_in), STDIN_FILENO) == -1) //error
+			if(dup2(fd[1], STDOUT_FILENO) == -1) //error
+			else if(close(fd[1]) == -1) //error
+			else {
+				execlp(pipes[index], pipes[index], NULL);
+			}
+			close(fd[1]);
+			close(fd_in);
+			pipeline(pipes,index+1, fd[0]);
+		}
+	}
+}//recursivepipes
