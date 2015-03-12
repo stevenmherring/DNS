e6f6110294572c70d8ee88455180a9e9ad659baf
Steven Herring <steven.herring@stonybrook.edu>
2015-03-12 02:18:33 -0400

    Removed quote restriction in piping...Woot, woot

--- lab2/redirHelper.c
+++ lab2/redirHelper.c
@@ -1,6 +1,5 @@
 void redirInput(char *input);
 void redirOutput(char *input);
-void recursivePipe(char *pipes[], int index, int fd_in);
 void execute_only_pipes(char* buffer);
 /**
 *redirInput small method for handling input redirection
@@ -21,41 +20,6 @@ void redirOutput(char *input) {
 	dup2(fd_in, STDOUT_FILENO);
 	close(fd_in);
 }
-
-/**
-*recursivePipe method uhhhh...i wrote it by hand so no comments
-**/
-void recursivePipe(char *pipes[], int index, int fd_in) {
-	write(1, "in pipes", 8);
-	if(pipes[index+1] == NULL) {
-		if(fd_in != STDIN_FILENO) {
-			if(dup2(fd_in, STDIN_FILENO) != -1) {
-				close(fd_in);
-			} else {
-			//space for error handling
-			}
-		}
-		execlp(pipes[index], pipes[index], NULL);
-	} else {
-		int fd[2];
-		pid_t cpid;
-		if((pipe(fd) == -1) || ((cpid = fork()) == -1)) {
-			//error handle, bad fork
-		}
-		if(cpid == 0) {
-			close(fd[0]);
-			if(dup2(fd_in, STDIN_FILENO) == -1) { } //error
-			if(dup2(fd[1], STDOUT_FILENO) == -1){ } //error
-			else if(close(fd[1]) == -1) { } //error
-			else {
-				execlp(pipes[index], pipes[index], NULL);
-			}
-			close(fd[1]);
-			close(fd_in);
-			recursivePipe(pipes,index+1, fd[0]);
-		}
-	}
-}//recursivepipes
 
 /* Executes the command 'buffer' assuming that doesn't contain redirections */
 void execute_only_pipes(char* buffer)
@@ -98,6 +62,13 @@ void execute_only_pipes(char* buffer)
                     /* If a parameter is ~, then replace it by /home/user */
                     if (!strcmp(temp, "~"))
                         strcpy(temp, "/home/user");
+										if((temp[0] == '"' && temp[strlen(temp-1)] == '"') || (temp[0] == '\'' && temp[strlen(temp-1)] =='\'')) {
+											char *ps;
+											for (ps = temp; *ps != '\0'; ps++) {
+    										*ps = *(ps+1);
+											}
+											temp[strlen(temp) - 1] = '\0';
+										}
                     cmdArgs[++aCount] = temp;
                 }
             }
@@ -129,8 +100,11 @@ void execute_only_pipes(char* buffer)
                 dup2(newPipe[1], 1);
                 close(newPipe[1]);
             }
-
-
+						int ind = 0;
+						while(cmdArgs[ind] != NULL) {
+							printf("arguments: %s\n", cmdArgs[ind]);
+							ind ++;
+						}
             if (execvp(cmdArgs[0], cmdArgs) == -1)
             {
                 printf("Error. Command not found: %s\n", cmdArgs[0]);
