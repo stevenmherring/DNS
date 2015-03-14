8250a22dc6c5697f4268c61cb0e6c6c7689633ae
Steven Herring <steven.herring@stonybrook.edu>
2015-03-13 23:51:07 -0400

    move folders around

---
+++ lab2/WorkingSwish.c
@@ -0,0 +1,276 @@
+/* CSE 306: Sea Wolves Interactive Shell */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include "mysyscall.h"
+#include "swishHelperFunctions.c"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+// Assume no input line will be longer than 1024 bytes
+#define MAX_INPUT_BUFF_BUFF 1024
+
+typedef enum {false, true} bool; //boolean enumerations
+
+extern char **environ;
+
+int main (int argc, char ** argv, char **envp) {
+
+  /********************************************************************
+  Command line parsing begins here
+  First check to see if -d is defined, and debugging messages need to be printed.
+  ********************************************************************/
+
+  putenv("SomeVariable=SomeValue");
+  int finished = 0;
+  char *prompt = "swish> ";
+  char cmd[MAX_INPUT_BUFF_BUFF];
+  char tokenBuff[MAX_INPUT_BUFF_BUFF];
+  char cmdTemp[MAX_INPUT_BUFF_BUFF];
+  char *EXIT_CMD =  "exit\n";
+  char cwd[MAX_INPUT_BUFF_BUFF];
+
+  /********************************************************************
+  Setting the commands that the shell recognizes
+  ********************************************************************/
+  char *commandsNL[] = { "ls\n","cd\n","pwd\n","printenv\n","putenv\n","cat\n"};
+  //char *commands[] = { "ls","cd","pwd","make"};
+
+  /*******************************************************************
+  Get the CWD path
+  Add a space and a null terminator to the path
+  Print the path
+  Print the 'swish' prompt
+  ********************************************************************/
+  int rv;
+  char *theCWD = getcwd(cwd,sizeof(cwd));
+  char *cursor;
+  cursor = theCWD + strlen(theCWD);
+  *cursor = ' ';
+  cursor++;
+  *cursor = '\0';
+  cursor =NULL ;
+  bool inRedir, outRedir;
+
+  rv = write(1, theCWD, strlen(theCWD));
+  rv = write(1, prompt, strlen(prompt));
+
+  while (!finished) {
+    char last_char;
+    int count;
+    //int iterator =0;
+    int pid=-1;
+
+    /********************************************************************
+    Removing this should make the only exit occur from the proper exit.
+    IE typing 'exit'.
+    *********************************************************************
+    if (!rv) {
+    finished = 1;
+    break;
+  } Ends while loop */
+
+
+  /* read and parse the input put it into CMD */
+  for(rv = 1, count = 0, cursor = cmd, last_char = 1;rv && (++count < (MAX_INPUT_BUFF_BUFF-1)) && (last_char != '\n'); cursor++) {
+    rv = read(0, cursor, 1);
+    last_char = *cursor;
+  }
+  *cursor = '\0';
+    if(!strncmp(cmd,EXIT_CMD,4)) {
+    finished = 1;
+    break;
+    return 0;
+  }
+  strcpy(tokenBuff,cmd);
+  char *tokenArr[50];
+  char *tokenArgs[50];
+  int counter = 0;
+  int counterArgs =0;
+  int flag =0;
+  char* token = strtok(tokenBuff, " ");
+
+  tokenArgs[counterArgs++] = token;
+  while (token) {
+    if (!strncmp(token,"-",1)){
+      flag = 1;
+	   tokenArgs[counterArgs++] = token;
+    } else
+    if (!strncmp(token,">",1)){
+      if(strlen(token) == 1){   
+        token = strtok(NULL, " ");
+      } //if char* length = 1
+      else {
+      } //else char* length > 1
+    }//if first char = > 
+    else {
+      tokenArr[counter++] = token;
+    } // else
+    token = strtok(NULL, " ");
+  }//while
+
+  int index = 0;
+  for(index = 0; tokenArr[counter - 1][index] != '\0'; index++) {
+    if(tokenArr[counter - 1][index] == '\n') {
+      tokenArr[counter - 1][index] = '\0';
+      break;
+    }
+  }
+
+  if (flag == 1){
+   for(index = 0; tokenArgs[counterArgs - 1][index] != '\0'; index++) {
+
+    if(tokenArgs[counterArgs - 1][index] == '\n') {
+      tokenArgs[counterArgs - 1][index] = '\0';
+      break;
+    }
+  }
+  }
+
+  tokenArr[counter] = NULL;
+  tokenArgs[counterArgs] = NULL;
+
+  //tokenArgs[1] = "-d";
+  /********************************************************************
+  Check the CMD string to see if the input matches any of the commands
+  found in the commandsNL array.
+  ********************************************************************/
+  //for(iterator= 0; iterator < 4; iterator++){
+
+  /********************************************************************
+  If the command is in the array, this line returns 0 so we ! to enter
+  ********************************************************************/
+  //if (!strncmp(commandsNL[iterator],cmd,2)){
+
+
+  /********************************************************************
+  IF the command is CD
+  ********************************************************************/
+  if (strncmp(commandsNL[1],cmd,2) == 0){
+    cursor = cmd; //Make a cursor to the CMD string
+    cursor += 3; // Move the cursor past 'cd ' onto the argument for CD
+    strcpy( cmdTemp , cursor ); // Copy the value at location into the cmdTemp string (this now holds CD arguments)
+    cursor = cmdTemp; // The copied over value still has '\n' so we make a cursor it
+    cursor += strlen(cmdTemp)-1; // Move the cursor to the end -1 (The location of the '\n')
+    *cursor = '\0';  // Replace the '\n' with Null terminator
+    if (strcmp( cmd , "cd\n") == 0){ // Base case CD needs to implement root CD
+      chdir(getenv("HOME"));
+      theCWD = getcwd (cwd,sizeof(cwd));
+      cursor = theCWD + strlen(theCWD);
+      *cursor = ' ';
+      cursor++;
+      *cursor = '\0';
+    } else
+    if ( chdir ( cmdTemp ) < 0) /* Fail case of opening directory */
+    {
+      printf("ERROR OPENING DIRECTORY:%s \n",cmdTemp);
+      return 1;
+    } else {
+      theCWD = getcwd (cwd,sizeof(cwd));
+      cursor = theCWD + strlen(theCWD);
+      *cursor = ' ';
+      cursor++;
+      *cursor = '\0';
+    }
+  }
+  //if the input is for a application (cat/ls) then we need to parse the arguments following it
+  else
+  if (!strncmp(cmd,"putenv",6)){
+    for(index = 0; tokenArr[1][index] != '\0'; index++) {
+      if(tokenArr[1][index] == '\n') {
+        tokenArr[1][index] = '\0';
+      break;
+      }
+    }
+    char *putEnvStr = malloc(strlen(tokenArr[1]));
+    strcpy(putEnvStr,tokenArr[1]);
+    if (putenv(putEnvStr) != 0){
+      return 123;
+    }
+  }
+  if ((pid = fork()) < 0){
+    printf("fork failed");
+    return 1;
+  }/* Fail case on fork */
+  if (pid == 0){
+    int j = 0;//, k = 0, l = 0;
+    //int in_index = 0, out_index = 0;
+    char input[MAX_INPUT_BUFF_BUFF];
+    char output[MAX_INPUT_BUFF_BUFF];
+    memset(input, '\0', strlen(input));
+    memset(output, '\0', strlen(output));
+
+    /*
+    *Check if the redirection operations were used, set flags.
+    */
+    j = getRedirTarget(cmd, output, '>');
+    if(j == 0 ) {
+      outRedir = true;
+    }
+    j = getRedirTarget(cmd, input, '<');
+    if(j == 0 ) {
+      inRedir = true;
+    }
+    ///for J search by char for < > | etc.
+    // execvp(commands[iterator],tokenArr);
+
+    /**
+    *Prior to exec we need to confirm if redirection was used, if so exec in a different manner
+    *Using tokens forces us to have spaces between the redirection arguments, this isnt how bash works
+    *So parsing character by character, which will have its own issues (file names w/ spaces for example)
+    **/
+    if(inRedir) {
+      //open/close FDs for in redirection
+      FILE *file_in = fopen(input, O_RDONLY);
+      int fd_in = file_in->_fileno;
+      dup2(fd_in, STDIN_FILENO);
+      close(fd_in);
+      inRedir = false;
+    }
+    if(outRedir) {
+      //open/close FDs for out redirection
+      FILE *file_out = fopen(output, "ab+");
+      int fd_out = file_out->_fileno;
+      dup2(fd_out, STDOUT_FILENO);
+      close(fd_out);
+      outRedir = false;
+    }
+    if (flag == 1){
+    printf("TokenArgs[0] %s \n",tokenArgs[0]);
+    printf("TokenArgs[1] %s \n",tokenArgs[1]);
+    printf("TokenArgs[2] %s \n",tokenArgs[2]);
+    write(1,"11111",5);
+    execvp(tokenArgs[0], tokenArgs);
+    } else 
+    write(1,"22222",5);
+    printf("TokenArr[0] %s \n",tokenArr[0]);
+    printf("TokenArr[1] %s \n",tokenArr[1]);
+    printf("TokenArr[2] %s \n",tokenArr[2]);
+    execvp(tokenArr[0],tokenArr);
+    //execvp(tokenArr[0], tokenArgs);
+    //cursor = cmd;
+    //*cursor = '\n';
+  } else {
+    /* in parent */
+    // int status;
+    waitpid(pid, NULL /*&status*/, WUNTRACED | WCONTINUED);
+  }
+
+  //}
+  //}
+  // Execute the command, handling built-in commands separately
+  // Just echo the command line for now
+  //    write(1, cmd, strnlen(cmd, MAX_INPUT_BUFF_BUFF));
+  /*******************************************************************
+  Print the CWD
+  Print the prompt
+  ********************************************************************/
+
+  rv = write(1, theCWD, strlen(theCWD));
+  rv = write(1, prompt, strlen(prompt));
+  memset(cmd,'\0',MAX_INPUT_BUFF_BUFF);
+} /* End while */
+return 0;
+}
---
+++ lab2/swishHelperFunctions.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+#define MAX_INPUT_BUFFER 1024
+
+/*
+Helper functions for swish
+*/
+int getRedirTarget(char *input, char *target, char op);
+char** strsplit(char* str, const char d);
+void removeSpaces(char* input, char **ret, int index);
+int findFlagD (char ** argv, int argc);
+
+int getArgs(char ** argv, int argc){
+	int i =0;
+	for (i =0; i<argc; i++){
+
+	printf("Argv[%d] : %s \n",i,argv[i]);
+	}
+	return 0;
+}
+
+
+/**
+*getRedirTarget takes a string and a character, returns the preceeding string
+*works if the string is connected to the character or separated by a space
+*returns 0 if successfully found the operand and string, else 1
+**/
+int getRedirTarget(char *input, char *target, char op) {
+	int i = 0, j = 0, k = 0;
+	int index = 0;
+	int found = 1;
+	for(i = 0; i < (strlen(input) - 1); i++) {
+		/**
+		*Search for target
+		**/
+		if(input[i] == op) {
+			found = 0;
+			if(input[i+1] == ' ') {
+				index = 2;
+			} else {
+				index = 1;
+			}
+			for(j = i+index; j < (strlen(input)); j++) {
+				if(input[j] != ' ' && input[j] != '<' && input[j] != '>') {
+					target[k] = input[j];
+					k++;
+				} else {
+					break;
+				}
+			}
+			target[k] = '\0';
+			break;
+		}//if op found
+		else {
+		}
+	}
+	return found;
+}
+
+/**
+*strsplit splits a string up and feeds it back as a 2d array
+**/
+char** strsplit(char* str, const char d) {
+	char** r = 0;
+	int count = 0;
+	char* temp = str;
+	char* ld = 0;
+
+	while(*temp)
+	{
+		if(d == *temp)
+		{
+			count++;
+			ld = temp;
+		}
+		temp++;
+	}
+	count += ld < (str + strlen(str) - 1);
+	count++;
+	r = malloc(sizeof(char*) * count);
+	if(r){
+		int index = 0;
+		char* token = strtok(str, &d);
+		while(token) {
+			*(r + index++) = strdup(token);
+			token = strtok(0, &d);
+		}
+		*(r + index) = 0;
+	}
+	return r;
+}
+
+void removeSpaces(char* input, char **ret, int index){
+	int rCount = 0;
+	char *rToke = strtok(input, " ");
+	while(rToke) {
+		ret[rCount++] = rToke;
+		rToke = strtok(NULL, " ");
+	}
+	//return ret;
+	//char *r = input;
+	//if(input[0] == ' ') {
+	//	for(i = 0, j = 1; i < strlen(input); i++, j++) {
+	//		r[i] = input[j];
+	//	}
+	//}
+
+
+
+
+}
+
+int printWolfie(){
+  char wolfieBuffer[255];
+  char *colors[] = {"\x1B[0m","\x1B[31m","\x1B[32m","\x1B[33m","\x1B[34m","\x1B[35m","\x1B[36m","\x1B[37m"};
+  FILE * fd;
+  if ((fd = fopen("catWolfieSAFE.txt","r")) < 0){
+    printf("Error opening");
+    return 1;
+  }
+  //fgets(wolfieBuffer,125,fd);
+  int index =1;
+  int iterator =0;
+  int colorCounter =0;
+  srand(time(NULL));
+  while(index != 0){
+    for (iterator =1;iterator < 24;iterator++){
+
+    printf("\n");
+    }
+    for (iterator =1;iterator < 24;iterator++){
+    colorCounter = rand() % 8;
+    index = (int) fgets(wolfieBuffer,125,fd);
+    if(index!=0){
+
+    printf("%s%s",colors[colorCounter] ,wolfieBuffer);
+    }
+    }
+
+    sleep(1);
+  }
+    return 0;
+}
+
+/*
+char* getCMDargs(char* command,char* bufferToParse){
+	int i =0;
+	char *cmdArgs[MAX_INPUT_BUFFER];
+	strcpy(cmdArgs,bufferToParse);
+	char *cursor = cmdArgs;
+	for (i=0; i<strlen(bufferToParse);i++){
+		if (*cursor == ' '){
+			cursor++;
+			printf("%s",cursor);
+		}
+		cursor+=i;
+	}
+}
+
+int parseExecFlags(char** commands,int commandsNLchoice, char ** argv, int argc){
+
+	//getCMDargs();
+
+    //execvp(commands[iterator],argv);
+	return 0;
+} */
+
+/* Process a ptr to an array of ptrs, and return 0 if -d is flag, 1 if it is not
+
+int findFlagD (char ** argv, int argc){
+
+	char* cursor;
+	int iterateArgv = 0;
+	for (iterateArgv = 1; iterateArgv < argc; iterateArgv++){
+
+	cursor = argv[iterateArgv];
+	while (cursor != NULL) {
+		if (!strcmp (cursor , "-d")){
+			return 0;
+		} else if (strlen (cursor) > 1 ){
+				cursor += strle(
+				if( strncmp ( cursor, ".sh", 3)){
+
+				}
+			}
+		cursor++;
+	}
+	}
+	return 1;
+}
+
+ Takes a buffer that contains the file for the script, a char* contains the arguments for the script and return whether the execution succeeded
+
+int execScript (char *scriptName, char *scriptArgs[]){
+
+	int returnValScript;
+	char *const currentEnv = NULL;
+	currentEnv = getenv("PATH");
+	if ( (returnValScript = execvpe(scriptName, scriptArgs, currentEnv) ) == -1){
+		printf("Error executing script");
+		return -1;
+	}
+
+}
+*/
