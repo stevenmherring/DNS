22f44af25c78171edc7151fa8957372b5a568323
Steven Herring <steven.herring@stonybrook.edu>
2015-03-25 12:00:03 -0400

    commit for update

---
+++ TEST/lab1/myhelpers.h
@@ -0,0 +1,40 @@
+/**
+ * Calculates the length of a '\0' terminated string.
+ * (the null character is not part of the length)
+ * @param str Pointer to a '\0' terminated string.
+ * @return Returns the length of the string. 
+ */
+int strlen(const char *str);
+
+/**
+ * Determines if two strings are exactly equal to each other.
+ * @param str1 Pointer to a '\0' terminated string.
+ * @param str2 Pointer to a '\0' terminated string.
+ * @return Returns 1 if equal, else 0.
+ */
+int strcmp(const char *str1, const char *str2);
+
+/**
+ * Copies the string from src into dst.
+ * @param dest Buffer to copy into.
+ * @param src String to copy.
+ * @param n Non-negative maximum about of bytes that can be copied from src.
+ * @return Returns the dst pointer.
+ */
+//char* strncpy(char *dst, const char *src, int n);
+void my_strcpy(char* str1, char* str2);
+
+/**
+ *Memset, replaces a requested number of characters in char with ASCII value input
+ *@param n Number of characters to be replaced
+ *@param src address
+ *@param c int ASCII value for char replacement (i.e 10 for \0)
+ **/
+ void str_memset(char *str, int c, int n);
+
+ /**
+ *ATOF TYPE OF FUNCTION
+ *Only converting string to int though
+ **/
+double my_atof(char *str);
+int my_strcmp(const char *str1, const char *str2);
---
+++ TEST/lab1/mysyscall.h
@@ -0,0 +1,109 @@
+#ifndef __MYSYSCALL_H__
+#define __MYSYSCALL_H__
+#include <asm/unistd.h>
+#include <errno.h>
+
+// Exercise 5: Your code here
+// Populate each of these functions with appropriate
+// assembly code for each number of system call arguments.
+//
+// Go ahead and fill in all 7 variants, as you will eventually
+// need them.
+//
+// Friendly advice: as you figure out the signature of a system
+// call, you might consider writing a macro for it for future reference, 
+// like:
+//
+// #define MY_GETPID(...) MY_SYSCALL...(...)
+
+#define MY_SYSCALL0(NUM)({\
+    int rv = -ENOSYS;\
+    __asm__ __volatile__ ("int $0x80" : "=a"(rv) : "a"(NUM));\
+    rv;\
+})
+
+#define MY_SYSCALL1(NUM, ARG1)\
+  ({\
+    int rv = -ENOSYS;\
+    __asm__ __volatile__ ("int $0x80" : "=a" (rv) : "a" (NUM), "b"(ARG1));\
+    rv;\
+  })
+
+#define MY_SYSCALL2(NUM, ARG1)\
+   ({\
+     int rv = -ENOSYS;\
+     __asm__ __volatile__ ("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1));\
+     rv;\
+   })
+   
+#define MY_SYSCALL3(NUM, ARG1, ARG2, ARG3)\
+   ({\
+     int rv = -ENOSYS;\
+     __asm__ __volatile__ ("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1), "c"(ARG2), "d"(ARG3));\
+     rv;\
+   })
+   
+#define MY_SYSCALL4(NUM, ARG1, ARG2, ARG3)    \
+   ({             \
+     int rv = -ENOSYS;          \
+     __asm__ __volatile__ ("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1),  "c"(ARG2), "d"(ARG3));\
+     rv;            \
+   })
+
+#define MY_SYSCALL5(NUM, ARG1, ARG2, ARG3)    \
+   ({             \
+     int rv = -ENOSYS;          \
+     __asm__ __volatile__ ("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1), "c"(ARG2), "d"(ARG3));\
+     rv;            \
+   })
+
+#define MY_SYSCALL6(NUM, ARG1)\
+   ({\
+    int rv = -ENOSYS;\
+    __asm__ __volatile__("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1));\
+    rv;\
+   })
+
+   #define MY_SYSCALL10(NUM, ARG1)\
+   ({\
+    int rv = -ENOSYS;\
+    __asm__ __volatile__("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1));\
+    rv;\
+  })
+
+#define MY_SYSCALL84(NUM, ARG1, ARG2)\
+    ({\
+    int rv = -ENOSYS;\
+    __asm__ __volatile__("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1), "c"(ARG2));\
+    rv;\
+    })
+   
+#define MY_SYSCALL20(NUM)\
+   ({\
+    int rv = -ENOSYS;\
+    __asm__ __volatile__("int $0x80"\
+    :"=a"(rv)\
+    :"a"(NUM)\
+    :);\
+    rv;\
+   })
+
+#define MY_SYSCALL33(NUM, ARG1, ARG2)\
+   ({\
+    int rv = -ENOSYS;\
+    __asm__ __volatile__("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1), "c"(ARG2));\
+   rv;\
+   })
+
+#define MY_SYSCALL141(NUM, ARG1, ARG2, ARG3)    \
+   ({             \
+     int rv = -ENOSYS;          \
+     __asm__ __volatile__ ("int $0x80" : "=a"(rv) : "a"(NUM), "b"(ARG1), "c"(ARG2), "d"(ARG3));\
+     rv;            \
+   })
+
+#endif // __MYSYSCALL_H__
+
+
+
+
